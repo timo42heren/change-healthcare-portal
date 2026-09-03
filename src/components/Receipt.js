4e538f3612611b198eed2b712a86177eaac22baa
timo42heren <timo42heren@outlook.com>
2026-09-03 17:59:40 +0530

    Create Receipt.js

---
+++ src/components/Receipt.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Receipt.css";
+
+function Receipt() {
+  return (
+    <div className="receipt">
+      <h3>Receipt Information</h3>
+      <p>Receipt details will appear here.</p>
+    </div>
+  );
+}
+
+export default Receipt;
